8064334a5e79c40561f9705a85cbe6578460897d
Jacob Albright <jacob.albright23@gmail.com>
2019-03-09 17:03:42 -0600

    added navigation and post components

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,9 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Open+Sans'}
+
     ]
   },
 
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,10 +1,31 @@
 <template>
   <div class="posts-page">
-    <ul>
-
-    </ul>
+    <section class="post-list">
+      <PostPreview 
+        :id="1" 
+        thumbnail="https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/cc_iStock-478639870_16x9.jpg?itok=1-jMc4Xv" 
+        titleText="All About The Woods"
+        previewText="kate moss"
+      />
+      <PostPreview 
+        :id="2" 
+        thumbnail="https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/cc_iStock-478639870_16x9.jpg?itok=1-jMc4Xv" 
+        titleText="Wood and Board"
+        previewText="mossy moss"
+      />
+    </section>
   </div>
 </template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+export default {
+  components: {
+    PostPreview,
+  },
+}
+</script>
+
 
 <style scoped>
 .posts-page {
@@ -12,4 +33,13 @@
   justify-content: center;
   align-items: center;
 }
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
 </style>
