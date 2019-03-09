571d307052f7fce5d36d2dcd7e99b5837711b483
Jacob Albright <jacob.albright23@gmail.com>
2019-03-09 15:56:26 -0600

    basic layout done

---
+++ pages/about/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p> Some text and stufgf This is really great </p>
+    <p> Thinaks for omoing </p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="posts-page">
+    <ul>
+
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
